8a4565f5e0f9f82591f92afbb7d50b1af5372551
Blehh13 <banubb2005@gmail.com>
2025-07-07 21:15:31 +0530

    Simplified SwipeClean App

--- frontend/js/main.js
+++
@@ -1,8 +0,0 @@
-function initApp() {
-  const hasStoragePermission = localStorage.getItem('hasStoragePermission');
-  if (!hasStoragePermission && window.location.pathname !== '/permissions.html') {
-    window.location.href = '/permissions.html';
-  }
-}
-
-window.addEventListener('load', initApp);
